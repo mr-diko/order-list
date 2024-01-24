b915b82a5db8703de200b3710e31872e4546487f
mr.Diko <mr.diko89@gmail.com>
2024-01-24 10:54:40 +0200

    Refactor: Extract Dialog Invocation to Separate Method

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -20,6 +20,36 @@ class _HomePageState extends State<HomePage> {
   ];
   final List<TextEditingController> _controllers = [];
 
+  showFilledFilds() {
+    String message = "";
+    for (var i = 0; i < productsList.length; i++) {
+      if (_controllers[i].text.isNotEmpty) {
+        message += "${productsList[i]}:${_controllers[i].text};\n";
+      }
+    }
+    showDialog(
+        context: context,
+        builder: (builder) {
+          return AlertDialog(
+            content: Column(
+              children: [
+                Text(message),
+                Row(
+                  children: [
+                    MaterialButton(
+                      onPressed: () {
+                        Clipboard.setData(ClipboardData(text: message));
+                      },
+                      child: const Text("Зберегти в буфер обміну"),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          );
+        });
+  }
+
   @override
   void dispose() {
     for (var element in _controllers) {
@@ -32,36 +62,12 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
         floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            String message = "";
-            for (var i = 0; i < productsList.length; i++) {
-              if (_controllers[i].text.isNotEmpty) {
-                message += "${productsList[i]}:${_controllers[i].text};\n";
-              }
-            }
-            showDialog(
-                context: context,
-                builder: (builder) {
-                  return AlertDialog(
-                    content: Column(
-                      children: [
-                        Text(message),
-                        Row(
-                          children: [
-                            MaterialButton(
-                              onPressed: () {
-                                Clipboard.setData(ClipboardData(text: message));
-                              },
-                              child: const Text("Зберегти в буфер обміну"),
-                            )
-                          ],
-                        ),
-                      ],
-                    ),
-                  );
-                });
-          },
-          child: const Icon(Icons.copy),
+          backgroundColor: Colors.blue,
+          onPressed: showFilledFilds,
+          child: const Icon(
+            Icons.copy,
+            color: Colors.white,
+          ),
         ),
         appBar: AppBar(
           backgroundColor: Colors.blue,
